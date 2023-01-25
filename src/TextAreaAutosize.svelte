24c5b4bec490ca2d442adc6a08464b861be6b2f6
Alexei Knyazev <52626785+alex-knyaz@users.noreply.github.com>
2023-01-25 08:50:43 +0300

    Added TextAreaAutosize

---
+++ src/TextAreaAutosize.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let value = "";
+  export let disabled = false;
+  export let no_border = false;
+</script>
+
+<div class="container" style={no_border ? "border: none;" : ""}>
+  <pre aria-hidden="true">{value + "\n"}</pre>
+  <textarea bind:value {disabled} on:change />
+</div>
+
+<style>
+  .container {
+    position: relative;
+    border: solid 1px;
+  }
+
+  pre,
+  textarea {
+    font-size: 1em;
+    line-height: 1;
+
+    overflow: hidden;
+    box-sizing: border-box;
+
+    min-height: auto;
+    padding: 0.5em;
+  }
+
+  textarea {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    resize: none;
+    outline: none !important;
+    border: none !important;
+  }
+</style>
